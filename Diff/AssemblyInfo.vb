9bca1a7fc396d9df8e92009f3d16ca7e1f227bf6
Jochen Wezel <jwezel@compumaster.de>
2020-12-14 18:52:00 +0100

    cleanup references + namespace-imports + added diff app for console

---
+++ Diff/AssemblyInfo.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("CompuMaster.Text.Diff - BSD license edition")>
+<Assembly: AssemblyDescription("Quickly dump a diff output to Console or to HTML")>
+<Assembly: AssemblyCompany("CompuMaster GmbH")>
+<Assembly: AssemblyProduct("CompuMaster Tools")>
+<Assembly: AssemblyCopyright("2017,2019,2020 CompuMaster GmbH")>
+<Assembly: AssemblyTrademark("camm")>
+<Assembly: CLSCompliant(False)>
+
+<Assembly: Guid("d3c5fcef-5a56-44b0-8c12-584a7de08b0b")>
+<Assembly: AssemblyVersion("2020.12.14.100")>
---
+++ Diff/Options.vb
@@ -0,0 +1,27 @@
+﻿Imports CommandLine
+
+Public Class Options
+
+    <CommandLine.Value(0, Required:=True,
+        HelpText:="Path of file 1")>
+    Public Property File1 As String
+
+    <CommandLine.Value(1, Required:=True,
+        HelpText:="Path of file 2")>
+    Public Property File2 As String
+
+    <[Option]("v", "UseHumanFriendlyControlChars", [Default]:=False,
+        HelpText:="Replace control chars by human-readable substitutions")>
+    Public Property HumanFriendlyControlChars As Boolean
+
+    Public Enum InputOutputModes As Integer
+        PlainText = 0
+        PlainTextComparisonWithHtmlOutput = 1
+        Html = 2
+    End Enum
+
+    <[Option]("m", "Mode", [Default]:=InputOutputModes.PlainText,
+              HelpText:="One of the following modes: PlainText, PlainTextComparisonWithHtmlOutput, Html")>
+    Public Property Mode As InputOutputModes
+
+End Class
