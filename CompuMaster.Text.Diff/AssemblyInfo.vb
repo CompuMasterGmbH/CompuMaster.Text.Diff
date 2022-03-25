cc0e2364df8e17a2ab9b48c2c3e560287a0213a1
Jochen Wezel <jwezel@compumaster.de>
2022-03-25 11:54:52 +0100

    BREAKING CHANGES:  renamed methods to better reflect their purpose
    added inline documentation for methods and arguments

--- CompuMaster.Text.Diff/AssemblyInfo.vb
+++ CompuMaster.Text.Diff/AssemblyInfo.vb
@@ -5,9 +5,9 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Quickly dump a diff output to System.Console or to HTML")>
 <Assembly: AssemblyCompany("CompuMaster GmbH")>
 <Assembly: AssemblyProduct("CompuMaster Tools")>
-<Assembly: AssemblyCopyright("2017,2019,2020,2021 CompuMaster GmbH")>
+<Assembly: AssemblyCopyright("2017-2022 CompuMaster GmbH")>
 <Assembly: AssemblyTrademark("camm")>
 <Assembly: CLSCompliant(False)>
 
 <Assembly: Guid("6e3937a8-0195-43b7-b94a-b070632ff07e")>
-<Assembly: AssemblyVersion("2021.01.28.100")>
+<Assembly: AssemblyVersion("2022.03.25.100")>
